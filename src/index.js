b36bf694612a70862d66276c28ce1dd20c2e2a89
hongfei <hongfei.ling@ctgtmo.com>
2019-04-19 17:16:00 +0800

    实现在线访问

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import {HashRouter} from "react-router-dom" // # 模式
-import {BrowserRouter} from "react-router-dom" // 普通模式
+import {HashRouter} from "react-router-dom" // # 模式
+// import {BrowserRouter} from "react-router-dom" // 普通模式
 import { createStore, applyMiddleware } from 'redux';
 import thunk from "redux-thunk"
 import { Provider } from 'react-redux';
@@ -12,11 +12,11 @@ import reducer from "./reducers/index";
 const store = createStore(reducer,applyMiddleware(thunk))
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
 
 
